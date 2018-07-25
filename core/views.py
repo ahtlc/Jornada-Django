5903ed54e5e222b535dd2354c5b06ffbd38a1d24
Arthur Henrique Tavares de Lyra Costa <ahtlc@ug4c11.windows.cin.ufpe.br>
2018-07-25 17:12:44 -0300

    datas vindo

--- core/views.py
+++ core/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .helpers import get_character_name, ip_info
+from datetime import datetime
 
 def index(request):
-    country = ip_info('country_name')
-    flag = ip_info('flag')
-    
     context = {
-        'country': country,
-        'flag': flag,
+        'now': datetime.now(),
     }
-    
     return render(request, 'index.html', context)
