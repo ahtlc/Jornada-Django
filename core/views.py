ecb37cb60a826faebfeefc7925a7d15192784af7
Arthur Henrique Tavares de Lyra Costa <ahtlc@ug4c11.windows.cin.ufpe.br>
2018-07-25 15:43:28 -0300

    aplicando api do ip

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .helpers import get_character_name, ip_info
 
-# Create your views here.
+def index(request):
+    country = ip_info('country_name')
+    flag = ip_info('flag')
+    flag_image = f'<img src="{flag}" width="120"/>'
+    return HttpResponse(f'Olá, direto de <b>{country}</b>{flag_image}')
