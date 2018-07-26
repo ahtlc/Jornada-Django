4ce47147c377399e66cde4cc514d00161e342ded
Arthur Henrique Tavares de Lyra Costa <ahtlc@ug4c11.windows.cin.ufpe.br>
2018-07-26 17:24:25 -0300

    1o model pronto

--- tuirer/settings.py
+++ tuirer/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'tuites',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -80,6 +83,8 @@ DATABASES = {
     }
 }
 
+# Users
+AUTH_USER_MODEL = 'users.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -118,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    '/var/www/static/',
+]
---
+++ tuites/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Tuite(models.Model):
+    content = models.CharField('Tuite', max_length=280)
+    author = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='tuites')
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author.username}: {self.content}'
+    
+    class Meta:
+        ordering = ('-content', )
